9dd25b7c823ca964c824c3cacbb853a9637baad6
Jair <irjaslazarna@ittepic.edu.mx>
2018-02-21 21:39:36 -0700

    Initial commit

---
+++ app/src/main/java/tdm2018/ittepic/edu/tdm2018_u1_reciclerview/Utilidades.java
@@ -0,0 +1,12 @@
+package tdm2018.ittepic.edu.tdm2018_u1_reciclerview;
+
+/**
+ * Created by Jair on 13/02/2018.
+ */
+
+public class Utilidades {
+    public static final int LIST=1;
+    public static final int GRID=2;
+
+    public static int visualizacion=LIST;
+}
